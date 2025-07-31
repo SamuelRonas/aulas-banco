c5842593021879e2f42c88dae1537f57c973ff11
joaoronas <joaoemanuelronasmendes@gmail.com>
2025-07-31 17:57:38 -0300

    um codigo que simula um banco e tem trs funcoes sacar depositar mostrar saldo

---
+++ codigosoma.js
@@ -0,0 +1,48 @@
+const prompt = require("prompt-sync")(); // Correto para Node.js com CommonJS
+
+function iniciar() {
+
+    var saldo = 0;
+
+    console.log("Oi, que bom te ver por aqui! O que deseja fazer hoje");
+
+    console.log("Digite 1 para Depositar, digite 2 para Sacar e 3 para Mostrar seu saldo");
+
+    var numeroUsuario = prompt("Digite sua opção: ");
+
+    if (numeroUsuario == 1) {
+        var deposito = prompt("Valor do deposito:  ")
+        depositar(deposito)
+    } else if (numeroUsuario == 2) {
+        var slk = prompt ('valor do saque')
+        sacar(slk)
+    } else if (numeroUsuario == 3) {
+        mostraSaldo()
+    } else {
+        console.log("Opção Invalida")
+    }
+
+
+    function depositar(valor) {
+        saldo = saldo + valor;
+        console.log(saldo)
+    }
+
+    function sacar(valor) {
+    saldo = saldo - valor;
+    console.log(saldo)
+
+
+    }
+
+
+    
+    function mostraSaldo() {
+        console.log(saldo)
+    }
+
+}
+
+
+
+iniciar();
